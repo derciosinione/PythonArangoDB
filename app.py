02ae042c476d74cf92a28b3fbee8d01c8185f7ae
derciosinione <derciosinione@gmail.com>
2021-08-10 04:42:19 +0100

    feat: Insert new data

--- app.py
+++ app.py
@@ -15,8 +15,13 @@ else:
   
 # studentsCollection = db.createCollection(name='Students')
 studentCll = db['Students']
-doc1 = studentCll.createDocument({'name': 'dercio derone', 'email': 'dercio@gmail.com'})
+doc1 = studentCll.createDocument({'name': 'Dercio Derone', 'email': 'dercio@gmail.com'})
+doc2 = studentCll.createDocument({'name': 'Paulo Lopes', 'email': 'paulo@gmail.com'})
+doc3 = studentCll.createDocument({'name': 'Anderson Francisco', 'email': 'anderson@gmail.com'})
 # doc1['name'] = 'Dercio Derone'
 # doc1['email'] = 'derciosinione@gmail.com'
+
+doc1.save(); doc2.save(); doc3.save()
+
 
 print(doc1)
