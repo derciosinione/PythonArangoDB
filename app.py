c11b16e69bd167190653a86785466a830b210d84
derciosinione <derciosinione@gmail.com>
2021-08-10 05:06:35 +0100

    feat: Get DB

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from os import name
+from typing import Any
 import pyArango.connection as adb
 
 
 cnx = adb.Connection(arangoURL='http://172.16.16.36:8529/',username='root', password='snirdb@2021')
 
+db = Any
 def getDB(str: name) -> bool:
-  if not cnx.hasDatabase(name='school'):
+  if not cnx.hasDatabase(name=name):
     op = input('This database does not exists, do you want to create? (Y/n)')
     if op == 'Y' or op == 'y':
-      db = cnx.createDatabase(name='school')
-      print('Database created')
+      db = cnx.createDatabase(name=name)
+    else: return False
   else:
-    db = cnx['school']
-    print('open new connection', db)
+    db = cnx[name]
+  return True
 
 def insertStudent() -> bool:
   # studentsCollection = db.createCollection(name='Students')
@@ -24,6 +26,4 @@ def insertStudent() -> bool:
   # doc1['name'] = 'Dercio Derone'
   # doc1['email'] = 'derciosinione@gmail.com'
   return True
-
-
 
