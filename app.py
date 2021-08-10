7189df5554cb57fa8e1ed4e613be796782642df2
derciosinione <derciosinione@gmail.com>
2021-08-10 04:26:37 +0100

    feat: verify db connection

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import pyArango.connection as adb
 
 cnx = adb.Connection(arangoURL='http://172.16.16.36:8529/',username='root', password='snirdb@2021')
 
-db = cnx.createDatabase(name='school')
 if not cnx.hasDatabase(name='school'):
   op = input('This database does not exists, do you want to create? (Y/n)')
-  if op is 'Y' or op is 'y':
-    cnx.hasDatabase(name='school')
-  else:
-    pass
+  if op == 'Y' or op == 'y':
+    db = cnx.createDatabase(name='school')
+    print('Database created')
+else:
+  db = cnx['school']
+  print('open new connection', db)
