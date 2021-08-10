16f87cc78a14f26a2a55256404af5811636c9528
derciosinione <derciosinione@gmail.com>
2021-08-10 04:53:49 +0100

    feat: Create functions

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pyArango.connection as adb
 
 cnx = adb.Connection(arangoURL='http://172.16.16.36:8529/',username='root', password='snirdb@2021')
 
+
 if not cnx.hasDatabase(name='school'):
   op = input('This database does not exists, do you want to create? (Y/n)')
   if op == 'Y' or op == 'y':
@@ -13,15 +14,16 @@ else:
   db = cnx['school']
   print('open new connection', db)
   
-# studentsCollection = db.createCollection(name='Students')
-studentCll = db['Students']
-doc1 = studentCll.createDocument({'name': 'Dercio Derone', 'email': 'dercio@gmail.com'})
-doc2 = studentCll.createDocument({'name': 'Paulo Lopes', 'email': 'paulo@gmail.com'})
-doc3 = studentCll.createDocument({'name': 'Anderson Francisco', 'email': 'anderson@gmail.com'})
-# doc1['name'] = 'Dercio Derone'
-# doc1['email'] = 'derciosinione@gmail.com'
+def insertStudent() -> bool:
+  # studentsCollection = db.createCollection(name='Students')
+  studentCll = db['Students']
+  doc1 = studentCll.createDocument({'name': 'Dercio Derone', 'email': 'dercio@gmail.com'})
+  doc2 = studentCll.createDocument({'name': 'Paulo Lopes', 'email': 'paulo@gmail.com'})
+  doc3 = studentCll.createDocument({'name': 'Anderson Francisco', 'email': 'anderson@gmail.com'})
+  doc1.save(); doc2.save(); doc3.save()
+  # doc1['name'] = 'Dercio Derone'
+  # doc1['email'] = 'derciosinione@gmail.com'
+  return True
 
-doc1.save(); doc2.save(); doc3.save()
 
 
-print(doc1)
