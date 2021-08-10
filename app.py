c177fda9ee299bc4c60f9d99083c76a57db4e137
derciosinione <derciosinione@gmail.com>
2021-08-10 04:55:43 +0100

    feat: Get DB

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import pyArango.connection as adb
 
 cnx = adb.Connection(arangoURL='http://172.16.16.36:8529/',username='root', password='snirdb@2021')
 
+def getDB(str: name) -> bool:
+  if not cnx.hasDatabase(name='school'):
+    op = input('This database does not exists, do you want to create? (Y/n)')
+    if op == 'Y' or op == 'y':
+      db = cnx.createDatabase(name='school')
+      print('Database created')
+  else:
+    db = cnx['school']
+    print('open new connection', db)
 
-if not cnx.hasDatabase(name='school'):
-  op = input('This database does not exists, do you want to create? (Y/n)')
-  if op == 'Y' or op == 'y':
-    db = cnx.createDatabase(name='school')
-    print('Database created')
-else:
-  db = cnx['school']
-  print('open new connection', db)
-  
 def insertStudent() -> bool:
   # studentsCollection = db.createCollection(name='Students')
   studentCll = db['Students']
