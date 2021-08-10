e8f14cbac59d07aa8be27d2e2f838397038d8ed5
derciosinione <derciosinione@gmail.com>
2021-08-10 05:41:04 +0100

    feat: Class DsArango

--- app.py
+++ app.py
@@ -21,22 +21,27 @@ class DsArango(object):
       self.db = self.cnx[dbname]
     return True
 
-  def insertStudent(self, dbCollection) -> bool:
+  def insertStudent(self) -> bool:
     # studentsCollection = db.createCollection(name='Students')
     dbCollection = self.db['Students']
-    doc1 = dbCollection.createDocument({'name': 'Dercio Derone', 'email': 'dercio@gmail.com'})
-    doc2 = dbCollection.createDocument({'name': 'Paulo Lopes', 'email': 'paulo@gmail.com'})
-    doc3 = dbCollection.createDocument({'name': 'Anderson Francisco', 'email': 'anderson@gmail.com'})
-    doc1.save(); doc2.save(); doc3.save()
+    # doc1 = dbCollection.createDocument({'name': 'Dercio Derone', 'email': 'dercio@gmail.com'})
+    # doc2 = dbCollection.createDocument({'name': 'Paulo Lopes', 'email': 'paulo@gmail.com'})
+    # doc3 = dbCollection.createDocument({'name': 'Anderson Francisco', 'email': 'anderson@gmail.com'})
+    # doc1.save(); doc2.save(); doc3.save()
     # doc1['name'] = 'Dercio Derone'
     # doc1['email'] = 'derciosinione@gmail.com'
+    docd = dbCollection.createDocument([{'name': 'Dercio Derone', 'email': 'dercio@gmail.com'}, 
+                                        {'name': 'Ana', 'email': 'ana@gmail.com'}])
+    docd.save()
+    print(docd)
     return True
   
   def insertManyStudent(self) -> bool:
     students = [('Elizandra', 'elizandra@gmail.com'), ('Angelica', 'angelica@gmail.com'),
                 ('Eduana', 'eduana@gmail.com'), ('Sinione', 'sinione@gmail.com')]
+    dbCollection = self.db['Students']
     for (name, email) in students:
-      doc = self.db
+      doc = dbCollection.createDocument()
 
 db = DsArango(dbUrl='http://172.16.16.36:8529/', username='root', password='snirdb@2021', dbname='school')
 print(db.insertStudent())
